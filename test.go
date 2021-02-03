1402caf58d5d00a522ea4f55876774d01cd24785
zukapasda <40371221@gm.nfu.edu.tw>
2021-02-03 10:49:36 +0800

    update test.go and add headers

--- test.go
+++ test.go
@@ -4,13 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/JustinSDK/goexample"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<a href=https://www.youtube.com/?gl=TW&hl=zh-tw >8787878</a>"))
+type Person struct {
+	name string
+	age  uint
+}
+
+var data = "58444"
+
+func httpc() {
+	http.HandleFunc("/root", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Write([]byte(data))
+		//	w.Write([]byte())
 	})
-	fmt.Println("Aloha")
-	log.Println("server on")
+
+}
+
+func main() {
+
+	httpc()
+	fmt.Println("wake server")
+	log.Println("server on55")
 	log.Fatal(http.ListenAndServe(":4000", nil))
+	goexample.Hi()
+	goexample.Hello()
 }
